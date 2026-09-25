71bef78184bae4e66da0c388c13bdc38a46e5345
Bruno Januário <230269349+bruno-januario@users.noreply.github.com>
2026-09-24 21:35:34 -0300

    Create function to show search results in DOM and add main css file

--- js/main.js
+++ js/main.js
@@ -4,6 +4,31 @@ const searchField = document.getElementById('search-field');
 const searchBtn = document.getElementById('search-btn');
 const searchResult = document.getElementById('search-result');
 
+export function showRepositories(data) {
+    searchResult.innerHTML = '';
+    let items = data.items;
+
+    const htmlContent = items.map(item => `
+        <div class="flex">
+            <div class="repo-container">
+                <h2>Repositório: ${item.name}</h2>
+                <p class="repo-description">Descrição: ${item.description || 'Sem descrição'}</p>
+                <p>Linguagem principal: ${item.language || 'Indisponível'}</p>
+                <p>Número de estrelas: ${item.stargazers_count}</p>
+                <p>Para acessar, <a href="${item.html_url}" target="_blank">clique aqui!</a></p>
+            </div>
+            <div class="author-container flex">
+                <div class="name-pic-pair">
+                    <img class="author-pic" src="${item.owner.avatar_url}" alt="${'Sem foto'}">
+                    <p>${item.owner.login}</p>
+                </div>       
+            </div>
+        </div>
+        `).join('');
+
+    searchResult.innerHTML = htmlContent;
+}
+
 searchField.addEventListener('keyup', (event) => {
     if(event.key.toLowerCase() == 'enter') {
         let fieldValue = searchField.value.trim();
--- js/search.js
+++ js/search.js
@@ -1,7 +1,9 @@
+import { showRepositories } from "./main.js";
+
 export async function search(keyword) {
     let response = await fetch(`https://api.github.com/search/repositories?q=${keyword}&sort=stars&per_page=10`);
 
     let data = await response.json();
 
-    console.log(data);
+    showRepositories(data);
 }
